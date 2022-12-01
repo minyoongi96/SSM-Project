057f2ccc243a5079b02e3d3cdd378876ab8f3d2d
minyoongi96 <zoobon63@gmail.com>
2022-12-01 10:03:39 +0900

    Table 어노테이션 추가

--- SSM/Spring/SSM/src/main/java/com/mijung/SSM/entity/Broadcasting.java
+++ SSM/Spring/SSM/src/main/java/com/mijung/SSM/entity/Broadcasting.java
@@ -3,10 +3,12 @@ package com.mijung.SSM.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
+@Table
 @Data
 public class Broadcasting {
 	
--- SSM/Spring/SSM/src/main/java/com/mijung/SSM/entity/Users.java
+++ SSM/Spring/SSM/src/main/java/com/mijung/SSM/entity/Users.java
@@ -3,12 +3,14 @@ package com.mijung.SSM.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table
 @Data
 public class Users {
 	@Id           // pk
