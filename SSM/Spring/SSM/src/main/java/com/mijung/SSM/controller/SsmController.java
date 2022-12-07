a1d2e5796310f4c90bf5fa809984f9d433a4f4f1
minyoongi96 <zoobon63@gmail.com>
2022-12-07 19:07:30 +0900

    pull 받기 전 커밋

--- SSM/Spring/SSM/src/main/java/com/mijung/SSM/controller/SsmController.java
+++ SSM/Spring/SSM/src/main/java/com/mijung/SSM/controller/SsmController.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.mijung.SSM.entity.Broadcasting;
 import com.mijung.SSM.entity.Users;
@@ -43,5 +45,13 @@ public class SsmController {
 			
 			return "listTest";
 		}
+	}
+	
+	@RequestMapping(value = "/loadingDashboard.do/{bcSeq}", method=RequestMethod.GET)
+	public String loadingDashboard(@PathVariable("bcSeq") final Long bcSeq, Model model) {
+		Broadcasting bcVO = ssmService.BcFindByBcSeq(bcSeq);
+		model.addAttribute("bc", bcVO);
+		
+		return "dashBoard";
 	}
 }
